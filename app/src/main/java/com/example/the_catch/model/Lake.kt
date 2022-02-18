d2d468a617195aa448f277082028c877192839e4
coolical <kevinnwoo@gmail.com>
2022-02-18 10:21:33 -0600

    added locations

---
+++ app/src/main/java/com/example/the_catch/model/Lake.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.model
+
+import android.widget.ImageButton
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.annotation.TransitionRes
+
+data class Lake (@DrawableRes val imageResourceId: Int){
+
+}
